89443545c309a718f10d218fed5cfba76a659817
LianMary <liana.maria230604@gmail.com>
2025-06-29 22:51:45 -0300

    Configuração do Controler, alteração em application,properties, adição de arquivo Docker, .env  e adição de arquivos gitIgnore e gitAttributes

--- src/main/java/lianalopes/com/example/Gerador/de/QrCode/controler/QrCodeControler.java
+++ src/main/java/lianalopes/com/example/Gerador/de/QrCode/controler/QrCodeControler.java
@@ -2,6 +2,7 @@ package lianalopes.com.example.Gerador.de.QrCode.controler;
 
 import lianalopes.com.example.Gerador.de.QrCode.DTO.QrCodeGeneretorRequest;
 import lianalopes.com.example.Gerador.de.QrCode.DTO.QrCodeGeneretorResponse;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/qrCode")
 public class QrCodeControler {
+
+    private final QrCodeGenaratorService qrCodeService;
+
+    public QrCodeControler(QrCodeGeneratorService qrCodeGeneratorService){
+        this.qrCodeGeneratorService = qrCodeService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGeneretorResponse> generate(@RequestBody QrCodeGeneretorRequest request){
-        return null;
+        try{
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        }catch(Exception e){
+            System.out.println(e); 
+            return ResponseEntity.internalServerErro().build();
+        }
     }
 
 }
